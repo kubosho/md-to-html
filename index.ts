b1bc5d770e8d67fab72873c414aff941bf3234ee
Shota Kubota <ta2@o2p.jp>
2018-08-21 02:22:27 +0900

    feat: rewrited CLI entry point

--- index.ts
+++ index.ts
@@ -1,46 +1,82 @@
 import globby from 'globby';
 import { resolve as resolvePath } from 'path';
+import { unwrapOrFromMaybe } from 'option-t/lib/Maybe/unwrapOr';
 import { defineArguments } from './lib/define-arguments';
-import { defaultAction } from './lib/default-action';
-import { optionToAction } from './lib/option-to-action';
+import {
+  readFile,
+  writeFile,
+  readStdin,
+} from './lib/io';
+import { convertToHtml } from './lib/markdown-to-html-converter';
 import { reportFatalError } from './lib/report-fatal-error';
 
-const args = defineArguments();
-const input = args._;
+async function getAbsolutePaths(input: string[]): Promise<string[]> {
+  if (input.length === 0) {
+    return [];
+  }
 
-Promise.resolve()
-  .then(async () => {
-    if (input && input.length) {
-      const paths = await globby(input);
+  const paths = await globby(input);
 
-      if (paths.length === 0) {
-        reportFatalError('Input Error: You must pass a valid list of files to parse');
-      }
+  if (paths.length === 0) {
+    reportFatalError('Input Error: You must pass a valid list of files to parse');
+  }
 
-      return paths;
-    }
+  return paths.map((path: string) => resolvePath(path));
+}
 
-    return ['stdin'];
-  })
-  .then((inputs: string[]) => {
-    let paths = inputs;
-    const argv = defineArguments();
+async function readFileContent(path?: string): Promise<string> {
+  if (!path || path.length === 0) {
+    return await readStdin();
+  }
 
-    if (paths[0] !== 'stdin') {
-      paths = paths.map((path: string) => resolvePath(path));
-    }
+  return await readFile(path);
+}
 
-    if (!!argv.to) {
-      paths.forEach((path: string) => {
-        optionToAction(path, { to: argv.to });
-      });
-      return;
-    }
+async function readFileContents(paths: string[]): Promise<string[]> {
+  let fileContents = null;
 
-    paths.forEach((path: string) => {
-      defaultAction(path);
-    });
-  })
-  .catch((err: any) => {
+  try {
+    fileContents = await Promise.all(paths.map((path: string) => {
+      return readFileContent(path);
+    }));
+  } catch (err) {
     reportFatalError(err);
-  });
+  }
+
+  return fileContents;
+}
+
+async function writeHtmlString(content: string, outputPath?: string) {
+  const htmlString = convertToHtml(content);
+
+  if (!outputPath) {
+    process.stdout.write(htmlString);
+    return;
+  }
+
+  try {
+    await writeFile(outputPath, htmlString);
+  } catch (err) {
+    reportFatalError(err);
+  }
+}
+
+export async function main() {
+  const argv = defineArguments();
+  const input = unwrapOrFromMaybe(argv._, []);
+  const outputPath = argv.to;
+
+  const paths = await getAbsolutePaths(input);
+  let content = '';
+
+  try {
+    content = await readFileContent(paths[0]);
+  } catch (err) {
+    reportFatalError(err);
+  }
+
+  if (paths.length <= 1) {
+    writeHtmlString(content, outputPath);
+    return;
+  }
+}
