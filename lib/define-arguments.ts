be053d05b518674ffc12598f33cbd0525410c34a
Shota Kubota <ta2@o2p.jp>
2018-08-23 22:53:13 +0900

    feat: add outDir option

--- lib/define-arguments.ts
+++ lib/define-arguments.ts
@@ -4,12 +4,19 @@ import { version } from './version';
 export interface MdToHtmlArguments extends yargs.Arguments {
   version?: string;
   output?: string;
+  outDir?: string;
 }
 
 export function defineArguments(): MdToHtmlArguments {
-  return yargs.version(version).option('out', {
-    alias: 'o',
-    type: 'string',
-    desc: 'Output HTML file in specified path',
-  }).argv;
+  return yargs
+    .version(version)
+    .option('out', {
+      alias: 'o',
+      type: 'string',
+      desc: 'Output HTML file in specified path',
+    })
+    .option('outDir', {
+      type: 'string',
+      desc: 'Output HTML file in specified directory',
+    }).argv;
 }
