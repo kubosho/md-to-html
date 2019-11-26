34be2e77f28c81affc3634370244915a6390d467
Shota Kubota <ta2@o2p.jp>
2019-11-26 10:31:54 +0900

    chore(prettierrc): change indent policy for markdown file

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,13 @@ module.exports = {
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
+
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
 };
